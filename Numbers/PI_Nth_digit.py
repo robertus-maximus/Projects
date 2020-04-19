0b065b65b17321536861ff193175b97a7f30c287
robertus-maximus <57766006+robertus-maximus@users.noreply.github.com>
2020-04-19 01:51:51 -0400

    Done

--- Numbers/PI_Nth_digit.py
+++ Numbers/PI_Nth_digit.py
@@ -1,11 +1,12 @@
 # Find PI to the Nth Digit
 #   Enter a number and have the program generate PI up to that many decimal places.
 #   Keep a limit to how far the program will go.
-import numpy as np
+import math
+from decimal import Decimal
 
 
 def trim_pi(n):
-    pi = np.clongdouble(22 / 7)
+    pi = Decimal.from_float(math.pi)
     num = str(pi)
     digits = num.split('.')
     string = digits[0] + '.' + str(digits[1])[:n]
@@ -15,7 +16,7 @@ def trim_pi(n):
 def main():
     x = False
     while not x:
-        n = int(input("Enter a number and have the program generate PI up to that many decimal places:"))
+        n = int(input("Enter a number and have the program generate PI up to that many decimal places: "))
         if n > 200:
             print("Value is too large, please enter a smaller value")
         else:
