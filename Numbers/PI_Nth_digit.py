fdf1511f17984778c141e49fa806b7585d98b161
robertus-maximus <57766006+robertus-maximus@users.noreply.github.com>
2020-04-18 23:19:02 -0400

    Second Commit

---
+++ Numbers/PI_Nth_digit.py
@@ -0,0 +1,12 @@
+# Find PI to the Nth Digit
+#   Enter a number and have the program generate PI up to that many decimal places.
+#   Keep a limit to how far the program will go.
+
+
+def main():
+    n = input("Enter a number and have the program generate PI up to that many decimal places.")
+    print("{:.2f}
+
+
+if __name__ == "__main__":
+    main()
